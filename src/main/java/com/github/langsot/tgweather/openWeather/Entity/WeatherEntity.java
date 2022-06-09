a2048dae6f507e6aa2566dd85529c118adedbfae
Eugene Belezki <langsot1337@gmail.com>
2022-06-09 15:47:57 +0700

    2-nd. Add wind for result answer

--- src/main/java/com/github/langsot/tgweather/openWeather/Entity/WeatherEntity.java
+++ src/main/java/com/github/langsot/tgweather/openWeather/Entity/WeatherEntity.java
@@ -15,41 +15,70 @@ public class WeatherEntity {
     private String name;
     private List<Weather> weather;
     private Main main;
+    private Wind wind;
+
+    @Setter
+    @Getter
+    private static class Main {
+        private Double temp;
+        private Double feels_like;
+    }
+
+    @Getter
+    @Setter
+    private static class Weather {
+        private String description;
+        private String icon;
+    }
+
+    @Setter
+    @Getter
+    private static class Wind {
+        private Double speed;
+    }
 
     public String getCurrentSmile(String description) {
         return switch(description) {
             case "01d" -> "☀";
-            case "02d" -> "\uD83C\uDF24";
-            case "03d" -> "⛅";
-            case "04d" -> "\uD83C\uDF25";
-            case "05d" -> "\uD83C\uDF27";
-            case "06d" -> "\uD83C\uDF26";
-            case "07d" -> "⛈";
-            case "08d" -> "❄";
-            case "09d" -> "\uD83C\uDF2B";
+            case "02d", "02n" -> "\uD83C\uDF24";
+            case "03d", "03n" -> "⛅";
+            case "04d", "04n" -> "\uD83C\uDF25";
+            case "05d", "05n" -> "\uD83C\uDF27";
+            case "06d", "06n" -> "\uD83C\uDF26";
+            case "07d", "07n" -> "⛈";
+            case "08d", "08n" -> "❄";
+            case "09d", "09n" -> "\uD83C\uDF2B";
+            case "01n" -> "\uD83C\uDF19";
             default -> "";
         };
     }
 
+    public String getCurrentWind(Double speed) {
+        if (speed < 0.5) {
+            return "Штиль";
+        } else if (speed < 1.7) {
+            return "Тихий ветерок";
+        } else if (speed < 4.5) {
+            return "Легкий ветер";
+        } else if (speed < 10) {
+            return "Умеренный ветер";
+        } else if (speed < 14.2) {
+            return "Сильный ветер";
+        } else if (speed < 18.2) {
+            return "Очень сильный ветер";
+        } else if (speed < 21.5) {
+            return "Штормовой ветер";
+        } else return "Ураган";
+    }
+
     public String toString() {
         return name + " - "
-                + weather.get(0).getDescription() + " "
                 + getCurrentSmile(weather.get(0).getIcon())
-                + "\n\uD83C\uDF21Температура: " + main.temp
-                + "\nОщущается как: " + main.feels_like;
+                + " " + weather.get(0).getDescription()
+                + String.format(
+                        "\n\uD83C\uDF21Температура: %.1f°C  (feel - %.1f)" +
+                                "\n\uD83C\uDF2A %s: %.1f м/c" , main.temp, main.feels_like, getCurrentWind(wind.speed), wind.speed);
     }
 
-    @Setter
-    @Getter
-    private class Main {
-        private String temp;
-        private String feels_like;
-    }
 
-    @Getter
-    @Setter
-    private class Weather {
-        private String description;
-        private String icon;
-    }
 }
